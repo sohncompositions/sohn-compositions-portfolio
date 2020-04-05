d634447862656b6791bb9208a87f4cb3cfbce578
Ben Turner <bbgrabbag@gmail.com>
2020-04-05 15:27:39 -0600

    added auto-scrolling and category display

--- changelog.md
+++ changelog.md
@@ -1,9 +1,9 @@
 Release Version 1.1.0
 **4-4-2020**
-- Add Category to audio display (ex. "Category | Song Name")
+- Added auto scrolling to long audio track names
+- Added Category to audio display (ex. "Category | Song Name")
 - Add Prev/Next buttons to audio player
-- Add auto scrolling to long audio track names
-- About Me label
 - Email link interpolation
 - Speed up heroku build process
 - fix security alert
+- Configuration README
--- client/src/app/footer/footer.component.ts
+++ client/src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewChecked, ViewChild, ElementRef } from '@angular/core';
 import { AudioService } from '../services/audio.service';
 
 @Component({
@@ -6,6 +6,48 @@ import { AudioService } from '../services/audio.service';
     templateUrl: './footer.component.html',
     styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements AfterViewChecked {
+
+    scrolling: boolean;
+
+    @ViewChild('movieScrollWrapper') movieScrollWrapper: ElementRef<HTMLDivElement>;
+    @ViewChild('scrollingText') scrollingText: ElementRef<HTMLParagraphElement>;
+
     constructor(public audioService: AudioService) { }
+
+    ngAfterViewChecked() {
+        this.applyMovieScroll();
+    }
+
+    private applyMovieScroll() {
+        const comparison = this.isScrollTextTooWide();
+        if (this.shouldScrollTextUpdate(comparison)) {
+            this.setScrollingText(comparison);
+        }
+    }
+
+    private shouldScrollTextUpdate(widthComparison: boolean): boolean {
+        return widthComparison !== this.scrolling;
+    }
+
+    private setScrollingText(scrolling: boolean): Promise<boolean> {
+        return Promise.resolve().then(() => {
+            return this.scrolling = scrolling;
+        });
+    }
+
+    private isScrollTextTooWide(): boolean {
+        if (!this.movieScrollWrapper || !this.scrollingText) {
+            return false;
+        }
+        const wrapperWidth = this.movieScrollWrapper.nativeElement.offsetWidth;
+        const textWidth = this.scrollingText.nativeElement.offsetWidth;
+        return textWidth > wrapperWidth;
+    }
+
+    provideScrollClass(): string {
+        return this.scrolling ? 'scrolling' : '';
+    }
+
+    // grab references to wrapper and text. if width of text is longer than wrapper, add 'scroll' class
 }
