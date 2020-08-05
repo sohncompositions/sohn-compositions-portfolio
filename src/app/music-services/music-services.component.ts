05870bf3529b5dec5bf1c343022d056cbcd708e6
Ben Turner <bbgrabbag@gmail.com>
2020-08-05 15:36:27 -0600

    optimized seo with robots.txt, sitemap, and meta tags

---
+++ src/app/music-services/music-services.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-music-services',
+    templateUrl: './music-services.component.html',
+    styleUrls: ['./music-services.component.scss']
+})
+export class MusicServicesComponent {
+
+}
